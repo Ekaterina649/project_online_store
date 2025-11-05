fb814c475f00e9d4005f4004057fba82fffe5314
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-11-06 00:22:05 +0300

    feat: update views and URLs for new template structure

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,29 @@
+from http.client import HTTPResponse
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """Домашняя страница"""
+    return render(request,"catalog/home.html")
+
+
+def contacts(request):
+    """Страница контактов"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print(f"Новое сообщение от: {name}, телефон: {phone}, сообщение: {message}")
+
+
+        return render(request, 'catalog/contacts.html', {
+            'success': True,
+            'name': name,
+            'phone': phone,
+            'message': message
+        })
+
+    return render(request, 'catalog/contacts.html')
+
