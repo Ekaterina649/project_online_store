af277363a47349654b34b801de385877d20bc85b
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-11-23 17:18:01 +0300

    add new migrations

--- catalog/models.py
+++ catalog/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100,verbose_name='Название продукта')
     description = models.CharField(max_length=200,verbose_name="Описание",default='',null=True,blank=True)
-    image = models.ImageField(upload_to='photos',verbose_name='Фото')
+    image = models.ImageField(upload_to='photos',verbose_name='Фото',null=True,blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,verbose_name='категория',blank=True, null=True)
     price = models.DecimalField(max_digits=10,decimal_places=2,verbose_name='цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True)
