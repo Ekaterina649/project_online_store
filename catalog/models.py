759cd85596ae03eec02d439cf61b0cd1a7832d6d
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-11-25 20:49:33 +0300

    fix models

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200, verbose_name="Описание", default='', null=True, blank=True)
+    description = models.TextField(verbose_name="Описание категории", default='')
 
     def __str__(self):
         return self.name
@@ -15,7 +15,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100,verbose_name='Название продукта')
-    description = models.CharField(max_length=200,verbose_name="Описание",default='',null=True,blank=True)
+    description = models.TextField(verbose_name="Описание продукта", default='')
     image = models.ImageField(upload_to='photos',verbose_name='Фото',null=True,blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,verbose_name='категория',blank=True, null=True)
     price = models.DecimalField(max_digits=10,decimal_places=2,verbose_name='цена за покупку')
