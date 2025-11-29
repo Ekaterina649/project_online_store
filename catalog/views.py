d996dc15b875378ef0c17e00b7c3e1b9d7741e4b
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-11-30 00:08:30 +0300

    Implement homepage: product list, truncate description to 100 chars, correct image display

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
+
 
 def index(request):
     """Домашняя страница"""
-    return render(request,"catalog/home.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -24,3 +29,7 @@ def contacts(request):
 
     return render(request, 'catalog/contacts.html')
 
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_detail.html',context)
