54a304f36fb3914737262004ed45237d74d14dcb
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-12-02 22:39:11 +0300

    use clean and standard URLs

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name='post_list'),
+    path('post/create/', BlogCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+]
