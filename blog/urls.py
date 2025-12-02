230108623988b1693489f101a7161dba0d28b0cd
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2025-12-02 23:41:13 +0300

    fix URLs

--- blog/urls.py
+++ blog/urls.py
@@ -11,8 +11,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='post_list'),
-    path('post/create/', BlogCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('create/', BlogCreateView.as_view(), name='post_create'),
+    path('detail/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('edit/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('delete/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 ]
