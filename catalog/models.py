1ec6d4463a565879431bf4bf7e420ef2870894a5
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2026-01-09 00:15:41 +0300

    add field in Product model

--- catalog/models.py
+++ catalog/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2,verbose_name='цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(blank=True, null=True, default=False,help_text='Отметьте, чтобы опубликовать продукт')
 
     def __str__(self):
         return f'{self.name} {self.category} {self.price}'
