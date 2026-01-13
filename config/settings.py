e7e8e81c7c62b27a145a6339378168514e5353e2
Ekaterina Sarafanova <ekaterinasarayanova@gmail.com>
2026-01-13 23:17:25 +0300

    feat: add Redis cache support

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,10 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = 'catalog:product_list'
 LOGOUT_REDIRECT_URL = 'catalog:product_list'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
